ab722db0f494a330d9c69bf74cb09b41bc14580f
Abdoulrhman <abdoulrhman.elsaid@smarteducation.ae>
2024-11-03 12:34:16 +0200

    Fix config issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  // base: 'http://sah-platform-admin-cpdsfdf8dkacfqgy.canadacentral-01.azurewebsites.net/',
+  base: 'https://sah-platform-admin-cpdsfdf8dkacfqgy.canadacentral-01.azurewebsites.net/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), 
